c367bc5d6b628631051dc6d3e1f43acc6853b552
agnbart <polpumi@gmail.com>
2023-11-16 20:45:03 +0100

    ref: add code to interact with mongodb

--- server.ts
+++ server.ts
@@ -3,7 +3,12 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-
+    const record = await prisma.post.create({
+      data: {
+// data to create new record
+      },
+    });
+    console.log('Record created', record);
 }
 
 main()
